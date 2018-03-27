42431496ef659d06070503020c720874f2386fdd
Ian Blenke <ian@blenke.com>
2018-03-27 17:08:21 -0400

    Add ELASTICSEARCH_LICENSE support

--- scripts/setup-kibana.sh
+++ scripts/setup-kibana.sh
@@ -9,6 +9,16 @@ until curl -s $es_url -o /dev/null; do
     sleep 1
 done
 
+# Setup a license if one is available to use
+if [ -n "${ELASTICSEARCH_LICENSE}" ]; then
+  echo "${ELASTICSEARCH_LICENSE}" | base64 -d > license.json
+  until curl -s -u elastic:${ELASTIC_PASSWORD} http://elasticsearch:9200/_xpack/license?acknowledge=true -H "Content-Type: application/json" -d @license.json ;
+  do
+    sleep 2
+    echo Trying to install elasticsearch license. Retrying...
+  done
+fi
+
 # Set the password for the kibana user.
 # REF: https://www.elastic.co/guide/en/x-pack/6.0/setting-up-authentication.html#set-built-in-user-passwords
 until curl -s -H 'Content-Type:application/json' \
@@ -16,5 +26,5 @@ until curl -s -H 'Content-Type:application/json' \
      -d "{\"password\": \"${ELASTIC_PASSWORD}\"}"
 do
     sleep 2
-    echo Retrying...
+    echo Trying to set Kibana password. Retrying...
 done
